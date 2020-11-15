adab4d66c19947e4e4458608ce733afdad15ab8d
Bruno Sader <brunosader@hotmail.com>
2020-11-15 17:02:26 +0100

    added preproc to audio

--- audio/spectro_analysis.py
+++ audio/spectro_analysis.py
@@ -1,4 +1,4 @@
-import preprocessing
+from preprocessing import movingaverage
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from python_speech_features import mfcc, logfbank, get_filterbanks
@@ -6,19 +6,24 @@ import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
 
 if __name__ =='__main__' :
-    ffmpeg_extract_subclip("storage/tmp/audio.wav", 0, 3000, targetname="storage/tmp/res_audio.wav")
-
+    #ffmpeg_extract_subclip("storage/tmp/audio.wav", 0, 3000, targetname="storage/tmp/res_audio.wav")
     (rate,sig) = wav.read('storage/tmp/res_audio.wav')
-    mfcc_feat = mfcc(sig,rate, nfft=1103, nfilt=6)
-    fbank_feat = logfbank(sig,rate, nfft=4410, nfilt=6, winlen=0.1, winstep=0.05)
-    print(len(fbank_feat))
-    '''print('#')
-    print(len(fbank_feat[0]))
-    print('##')
-    print(get_filterbanks(nfft=1103))
-    print('###')
-    print(len(get_filterbanks(nfft=1103)))'''
-    #plt.plot(mfcc_feat[:, 0])
+    #mfcc_feat = mfcc(sig,rate, nfft=1103, nfilt=6)
+    fbank_feat = logfbank(sig,rate,nfft=4410,nfilt=6,winlen=0.1,winstep=0.05)
+    #plt.plot(mfcc_feat)
     #plt.show()
-    plt.plot(fbank_feat)
+    #plt.plot(fbank_feat)
+    #plt.show()
+    fbank_feat_av_0 = movingaverage(fbank_feat[:, 0], 100)
+    fbank_feat_av_1 = movingaverage(fbank_feat[:, 1], 100)
+    fbank_feat_av_2 = movingaverage(fbank_feat[:, 2], 100)
+    fbank_feat_av_3 = movingaverage(fbank_feat[:, 3], 100)
+    fbank_feat_av_4 = movingaverage(fbank_feat[:, 4], 100)
+    fbank_feat_av_5 = movingaverage(fbank_feat[:, 5], 100)
+    plt.plot(fbank_feat_av_0)
+    plt.plot(fbank_feat_av_1)
+    plt.plot(fbank_feat_av_2)
+    plt.plot(fbank_feat_av_3)
+    plt.plot(fbank_feat_av_4)
+    plt.plot(fbank_feat_av_5)
     plt.show()
