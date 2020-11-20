231b7462e9ac3cc90d22e125b88be7a3d98e07c1
Bruno Sader <brunosader@hotmail.com>
2020-11-20 14:59:29 +0100

    WIP noise reduction

--- audio/speech_analysis.py
+++ audio/speech_analysis.py
@@ -3,9 +3,16 @@
 #---------------------------
 
 import noisereduce as nr
-# load data
-rate, data = wavfile.read("../storage/tmp/audio.wav")
-# select section of data that is noise
-noisy_part = wavfile.read("../storage/tmp/crowd_sample.wav")
-# perform noise reduction
-reduced_noise = nr.reduce_noise(audio_clip=data, noise_clip=noisy_part, verbose=True)
+import librosa
+
+if __name__ == '__main__' :
+
+    print('running ...')
+
+    # load data
+    data, rate = librosa.load("storage/tmp/audio.wav")
+    # select section of data that is noise
+    noisy_part, noisy_rate = librosa.load("storage/tmp/crowd_sample.wav")
+    # perform noise reduction
+    reduced_noise = nr.reduce_noise(audio_clip=data, noise_clip=noisy_part, verbose=True)
+    librosa.output.write_wav('storage/tmp/reduced_audio.wav', reduced_noise, rate)
