a432f0cccaa1fe9c95915fdda666d5c50120fcfd
Ahmed Abdel Rahman <ahmed.abdel_rahman@hotmail.com>
2020-11-20 14:28:55 +0100

    noisereduce test

--- audio/speech_analysis.py
+++ audio/speech_analysis.py
@@ -2,3 +2,10 @@
 # Spectral Gating Noise Reduction
 #---------------------------
 
+import noisereduce as nr
+# load data
+rate, data = wavfile.read("../storage/tmp/audio.wav")
+# select section of data that is noise
+noisy_part = wavfile.read("../storage/tmp/crowd_sample.wav")
+# perform noise reduction
+reduced_noise = nr.reduce_noise(audio_clip=data, noise_clip=noisy_part, verbose=True)
